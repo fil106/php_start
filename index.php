378205cfbce9f576d45714944ffa6cf08d51d1d1
Filipp <fil106@mail.ru>
2018-03-22 17:09:00 +0300

    Точка входа на сайт, начал писать

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+	/** FRONT CONTROLLER **/
+
+	//1. Общие настройки
+
+	//включаем отображение ошибок для диагностики
+	ini_set('display_errors', 1);
+	error_reporting(E_ALL);
+
+	//2. Подключение файлов системы
+
+	//определяем корневую директорию сайта в константу ROOT
+	define('ROOT', dirname(__FILE__));
+	require_once(ROOT.'/components/Route.php');
+
+	//3. Установка соединения с БД
+
+	//4. Вызов Router
