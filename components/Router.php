d1980aa386bc544d824be0b7ce83272326d2ad99
Filipp <fil106@mail.ru>
2018-03-22 20:04:25 +0300

    Commit

--- components/Router.php
+++ components/Router.php
@@ -6,10 +6,27 @@
 
 		public function __construct() {
 
+			$routerPath = ROOT.'/config/routes.php';
+			$this->routes = include($routerPath);
+
 		}
 
 		public function run() {
-			echo 'Method run was started';
+
+			// Получить строку запроса
+			if (!empty($_SERVER['REQUEST_URI'])) {
+				$uri = trim($_SERVER['REQUEST_URI'], '/');
+			}
+			echo $uri;
+
+			// Проверить наличие такого запроса в routes.php
+
+			// Если есть совпадение, определить какой контроллер и action обрабатывают запрос
+
+			// Подключить файл класса-контроллера
+
+			// Создать объект, вызвать метод (т.е. action)
+
 		}
 
 	}
